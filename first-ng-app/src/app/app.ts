270a2b9b74d9fc1cccda2318a2ad35108cf2dfcc
Gaurav Singh <35394758+gauravsingh890@users.noreply.github.com>
2026-03-31 17:35:37 +0000

    first ng app

---
+++ first-ng-app/src/app/app.ts
@@ -0,0 +1,26 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Header } from './components/header/header';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Home, Header],
+  template: `
+   <app-header />
+   <main>
+      <app-home />
+   </main>
+  `,
+  styles: [
+    `
+      main {
+        padding : 16px;
+      }
+    `
+  ],
+})
+export class App {
+  protected readonly title = signal('World!!!');
+}
